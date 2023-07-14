65d2ffe1519377912dedfb38e5a64a560ecf3dab
jackwill99 <mr.wailwinphyo.mu.phys@gmail.com>
2023-07-14 16:21:23 +0630

    export RTCViewoView and RTCVideoViewObjectFit

--- lib/jack_rtc_call.dart
+++ lib/jack_rtc_call.dart
@@ -5,6 +5,8 @@ library jack_rtc_call;
 /// Interface of the webRTC
 
 export "package:flutter_webrtc/src/native/rtc_video_renderer_impl.dart";
+export "package:flutter_webrtc/src/native/rtc_video_view_impl.dart";
+export "package:webrtc_interface/src/enums.dart";
 export "package:webrtc_interface/src/media_stream.dart";
 export "package:webrtc_interface/src/rtc_data_channel.dart";
 export "package:webrtc_interface/src/rtc_ice_candidate.dart";
